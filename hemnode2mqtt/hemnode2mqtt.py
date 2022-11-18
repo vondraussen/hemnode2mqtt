6208b5984fa6178d67cf8cddc66a1ef1d4481626
vondraussen <vondraussen@users.noreply.github.com>
2022-11-18 13:55:45 +0100

    add github action refactor add tests

--- hemnode2mqtt/hemnode2mqtt.py
+++ hemnode2mqtt/hemnode2mqtt.py
@@ -6,78 +6,103 @@ import paho.mqtt.client as mqtt
 import os
 from dotenv import load_dotenv
 
+
 def getInt16(data1, data2):
-  return ctypes.c_short(data1 + (data2 * 256)).value
+    return ctypes.c_short(data1 + (data2 * 256)).value
+
 
 def getBattery(data1, data2):
-  raw = ctypes.c_ushort(data1 + (data2 * 256)).value
-  return round((raw * 3020) / 1024)
+    raw = ctypes.c_ushort(data1 + (data2 * 256)).value
+    return round((raw * 3020) / 1024)
+
 
 def decodeMsg(data):
-  msg = {}
-  if data[0] != 4 and data[0] != 5:
-    return None
+    msg = {}
+    if data[0] != 0 and data[0] != 4 and data[0] != 5:
+        return None
 
-  msg['id'] = data[0]
-  msg['temp1'] = getInt16(data[4], data[5]) / 10
-  msg['temp2'] = getInt16(data[6], data[7]) / 10
-  msg['battery'] = getBattery(data[12], data[13])
+    if data[0] == 0 and len(data) != 7:
+        return None
 
-  return msg
+    msg['id'] = data[0]
 
-def parseToInfluxLine(data):
-  decodeMsg(data)
+    if msg['id'] == 4 or msg['id'] == 5:
+        msg['temp1'] = getInt16(data[4], data[5]) / 10
+        msg['temp2'] = getInt16(data[6], data[7]) / 10
+        msg['battery'] = getBattery(data[12], data[13])
+        msg['rssi'] = -(data[14]/2)
 
-def hemnoded():
-  load_dotenv()
-  MQTT_USER = os.getenv('HEMNODE2MQTT_MQTT_USER')
-  MQTT_PW = os.getenv('HEMNODE2MQTT_MQTT_PW')
-  MQTT_CLIENT_ID = os.getenv('HEMNODE2MQTT_MQTT_CLIENT_ID', default='hemnode2mqtt')
-  MQTT_BROKER = os.getenv('HEMNODE2MQTT_MQTT_BROKER', default='localhost')
-  MQTT_PORT = os.getenv('HEMNODE2MQTT_MQTT_PORT', default=8883)
-  MQTT_CA_CERT = os.getenv('HEMNODE2MQTT_MQTT_CA_CERT', default='/etc/ssl/certs/ca-certificates.crt')
-  MQTT_TOPIC = os.getenv('HEMNODE2MQTT_MQTT_TOPIC', default='homey/yourHome')
-  INFLUX_MEASUREMENT_TAG = os.getenv('HEMNODE2MQTT_INFLUX_MEASUREMENT_TAG', default='homes,Location=yourHome')
-  SERIAL_DEV = os.getenv('HEMNODE2MQTT_SERIAL_DEV', default='/dev/ttyUSB0')
+    if msg['id'] == 0:
+        msg['temp1'] = getInt16(data[1], data[2]) / 100
+        msg['humi'] = getInt16(data[3], data[4]) / 10
 
-  mqttClient = mqtt.Client(client_id=MQTT_CLIENT_ID, transport="tcp")
-  mqttClient.tls_set(ca_certs=MQTT_CA_CERT, tls_version=ssl.PROTOCOL_TLS)
-  mqttClient.username_pw_set(MQTT_USER, password=MQTT_PW)
-  mqttClient.connect(MQTT_BROKER, MQTT_PORT, 60)
-  mqttClient.loop_start()
+    return msg
 
-  ser = serial.Serial(SERIAL_DEV, 38400, timeout=0.2)
 
-  while True:
-    line = ser.read_until('\r\n')
-    if line == b'':
-      continue
+def parseToInfluxLine(msg):
+    INFLUX_MEASUREMENT_TAG = os.getenv(
+        'HEMNODE2MQTT_INFLUX_MEASUREMENT_TAG', default='homes,Location=yourHome')
 
-    try:
-      msg = decodeMsg(line)
-    except:
-      print('decode error', line)
-
-    if msg is None:
-      continue
-
+    messages = []
     if msg['id'] == 4:
-      influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=outside "
-      influxInline += f"Temperature={msg['temp1']}"
-      influxInline += f",Battery={msg['battery']}"
-      mqttClient.publish(MQTT_TOPIC, influxInline)
+        influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=outside "
+        influxInline += f"Temperature={msg['temp1']}"
+        influxInline += f",Battery={msg['battery']}"
+        messages.append(influxInline)
+        # mqttClient.publish(MQTT_TOPIC, influxInline)
 
     if msg['id'] == 5:
-      influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=garage "
-      influxInline += f"Temperature={msg['temp2']}"
-      influxInline += f",Battery={msg['battery']}"
-      mqttClient.publish(MQTT_TOPIC, influxInline)
-      influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=battery "
-      influxInline += f"Temperature={msg['temp1']}"
-      influxInline += f",Battery={msg['battery']}"
-      mqttClient.publish(MQTT_TOPIC, influxInline)
+        influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=garage "
+        influxInline += f"Temperature={msg['temp2']}"
+        influxInline += f",Battery={msg['battery']}"
+        # mqttClient.publish(MQTT_TOPIC, influxInline)
+        messages.append(influxInline)
+        influxInline = f"{INFLUX_MEASUREMENT_TAG},Room=battery "
+        influxInline += f"Temperature={msg['temp1']}"
+        influxInline += f",Battery={msg['battery']}"
+        # mqttClient.publish(MQTT_TOPIC, influxInline)
+        messages.append(influxInline)
 
-  mqttClient.disconnect
+    return messages
 
-if __name__ == '__main__':
-  hemnoded()
+
+def hemnoded():
+    load_dotenv()
+    MQTT_USER = os.getenv('HEMNODE2MQTT_MQTT_USER')
+    MQTT_PW = os.getenv('HEMNODE2MQTT_MQTT_PW')
+    MQTT_CLIENT_ID = os.getenv(
+        'HEMNODE2MQTT_MQTT_CLIENT_ID', default='hemnode2mqtt')
+    MQTT_BROKER = os.getenv('HEMNODE2MQTT_MQTT_BROKER', default='localhost')
+    MQTT_PORT = os.getenv('HEMNODE2MQTT_MQTT_PORT', default=8883)
+    MQTT_CA_CERT = os.getenv('HEMNODE2MQTT_MQTT_CA_CERT',
+                             default='/etc/ssl/certs/ca-certificates.crt')
+    MQTT_TOPIC = os.getenv('HEMNODE2MQTT_MQTT_TOPIC', default='homey/yourHome')
+    INFLUX_MEASUREMENT_TAG = os.getenv(
+        'HEMNODE2MQTT_INFLUX_MEASUREMENT_TAG', default='homes,Location=yourHome')
+    SERIAL_DEV = os.getenv('HEMNODE2MQTT_SERIAL_DEV', default='/dev/ttyUSB0')
+
+    mqttClient = mqtt.Client(client_id=MQTT_CLIENT_ID, transport="tcp")
+    mqttClient.tls_set(ca_certs=MQTT_CA_CERT, tls_version=ssl.PROTOCOL_TLS)
+    mqttClient.username_pw_set(MQTT_USER, password=MQTT_PW)
+    mqttClient.connect(MQTT_BROKER, MQTT_PORT, 60)
+    mqttClient.loop_start()
+
+    ser = serial.Serial(SERIAL_DEV, 38400, timeout=0.2)
+
+    while True:
+        line = ser.read_until('\r\n')
+        if line == b'':
+            continue
+
+        try:
+            msg = decodeMsg(line)
+        except:
+            print('decode error', line)
+
+        if msg is None:
+            continue
+
+        for influx in parseToInfluxLine(msg):
+            mqttClient.publish(MQTT_TOPIC, influx)
+
+    mqttClient.disconnect
--- test_hemnode2mqtt.py
+++ test_hemnode2mqtt.py
@@ -1,6 +1,7 @@
 from hemnode2mqtt import hemnode2mqtt
 import pytest
 
+
 @pytest.mark.parametrize("data1, data2, expected", [
     (0, 1, 256),
     (0, 256, 0),
@@ -10,6 +11,7 @@ import pytest
 def test_getInt16(data1, data2, expected):
     result = hemnode2mqtt.getInt16(data1, data2)
     assert result == expected, f"Should be {expected}"
+
 
 @pytest.mark.parametrize("data1, data2, expected", [
     (0, 1, 755),
@@ -21,27 +23,45 @@ def test_getBattery(data1, data2, expected):
     result = hemnode2mqtt.getBattery(data1, data2)
     assert result == expected, f"Should be {expected}"
 
+
 @pytest.mark.parametrize("data, expected", [
-    (b'\x00', None),
-    (b'\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 4, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf1\x01', {'battery': 1466, 'id': 5, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 10.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': -12.9, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 0, 'temp2': -12.9}),
+    (b'\x00\r\n', None),
+    (b'\x01\r\n', None),
+    (b'\x04\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x9e\r\n',
+     {'battery': 0, 'id': 4, 'temp1': 0.0, 'temp2': 0.0, 'rssi': -79.0}),
+    (b'\x04\x0c\x00\x00\x1c\x00\xff\xff\xff\xff\x02\x12\xa3\x01\x9e\r\n', {
+     'battery': 1236, 'id': 4, 'temp1': 2.8, 'temp2': -0.1, 'rssi': -79.0}),
+    (b'\x04\x0c\x00\x00\x1c\x00\xff\xff\xff\xff\x02\x13\xa4\x01\x9e\r\n', {
+     'battery': 1239, 'id': 4, 'temp1': 2.8, 'temp2': -0.1, 'rssi': -79.0}),
+    (b'\x05\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xb6\r\n',
+     {'battery': 0, 'id': 5, 'temp1': 0.0, 'temp2': 0.0, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf1\x01\xb6\r\n', {
+     'battery': 1466, 'id': 5, 'temp1': 0.0, 'temp2': 0.0, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00\x00\xb6\r\n',
+     {'battery': 0, 'id': 5, 'temp1': 10.0, 'temp2': 0.0, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00\x00\x00\xb6\r\n', {
+     'battery': 0, 'id': 5, 'temp1': -12.9, 'temp2': 0.0, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00\xb6\r\n',
+     {'battery': 0, 'id': 5, 'temp1': 0, 'temp2': -12.9, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x9c\x00\x9c\x00\xff\xff\x02\xd5\xb4\x01\xb6\r\n', {
+     'battery': 1286, 'id': 5, 'temp1': 15.6, 'temp2': 15.6, 'rssi': -91.0}),
+    (b'\x05\x0c\x00\x00\x9c\x00\x9c\x00\xff\xff\x02\xd6\xc3\x01\xb5\r\n', {
+     'battery': 1330, 'id': 5, 'temp1': 15.6, 'temp2': 15.6, 'rssi': -90.5}),
+    (b'\x00\xd0\x07\xd6\x01\r\n', {'id': 0, 'humi': 47.0, 'temp1': 20.0}),
+    (b'\x00\xd0\x07\xd7\x01\r\n', {'id': 0, 'humi': 47.1, 'temp1': 20.0}),
+    # (b'\x04\x0c\x00\x00\x1b\x00\xff\xff\xff\xff\x02!\x9d\x01\x9c\r\n\x04\x0c\x00\x00\x1b\x00\xff\xff\xff\xff\x02!\x9d\x01\x9c\r\n\x04\x0c\x00\x00\x1b\x00\xff\xff\xff\xff\x02!\x9d\x01\x9d\r\n', None),
 ])
 def test_decodeMsg(data, expected):
     result = hemnode2mqtt.decodeMsg(data)
     assert result == expected, f"Should be {expected}"
 
+
 @pytest.mark.parametrize("data, expected", [
-    (b'\x00', None),
-    (b'\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 4, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf1\x01', {'battery': 1466, 'id': 5, 'temp1': 0.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x64\x00\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 10.0, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': -12.9, 'temp2': 0.0}),
-    (b'\x05\x00\x00\x00\x00\x00\x7f\xff\x00\x00\x00\x00\x00\x00', {'battery': 0, 'id': 5, 'temp1': 0, 'temp2': -12.9}),
+    ({'battery': 0, 'id': 4, 'temp1': 0.0, 'temp2': 0.0},
+     ['homes,Location=yourHome,Room=outside Temperature=0.0,Battery=0']),
+    ({'battery': 0, 'id': 5, 'temp1': 10.0, 'temp2': -20.0},
+     ['homes,Location=yourHome,Room=garage Temperature=-20.0,Battery=0',
+     'homes,Location=yourHome,Room=battery Temperature=10.0,Battery=0', ]),
 ])
 def test_parseToInfluxLine(data, expected):
     result = hemnode2mqtt.parseToInfluxLine(data)
